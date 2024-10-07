cfe075fc9f74ddfffad400c3d9ea35cd7a85de15
en2000 <162282381+s108003617@users.noreply.github.com>
2024-10-07 19:32:17 +0800

    完成

---
+++ js/script-zh-tw.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+  strings: [" ", " 朱嘉豪", " Jerry"],
+  typeSpeed: 200,
+  backSpeed: 100,
+  loop: true,
+});
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,3 +1,15 @@
+/* nav colors */
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", function () {
+    // 移除所有連結的 active 類
+    document
+      .querySelectorAll(".nav a")
+      .forEach((a) => a.classList.remove("active"));
+
+    // 將當前點擊的連結設置為 active
+    this.classList.add("active");
+  });
+});
 /* toggle style switcher */
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
@@ -33,4 +45,20 @@ window.addEventListener("load", () => {
   } else {
     dayNight.querySelector("i").classList.add("fa-moon");
   }
+});
+/* change language */
+// 定義一個通用的重定向函數
+const changeLanguage = (url) => {
+  window.location.href = url;
+};
+
+// 獲取語言選項
+const languages = document.querySelectorAll("[data-lang]");
+
+// 為每個語言選項添加事件監聽器
+languages.forEach((lang) => {
+  lang.addEventListener("click", () => {
+    const url = lang.getAttribute("data-lang");
+    changeLanguage(url);
+  });
 });
